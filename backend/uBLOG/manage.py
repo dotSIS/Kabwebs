8ebbb07e5e9203302399a954781400fd93c6e4cd
dotSIS <sjezzrey@gmail.com>
2022-05-02 10:29:08 +0800

    proper restructure + virtual environment

--- backend/uBLOG/manage.py
+++ backend/uBLOG/manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Kabwebs.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'uBLOG.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
