ce36cd4c11590c99c734930890aca624526d8899
dotSIS <sjezzrey@gmail.com>
2022-05-06 22:13:09 +0800

    added category serializer

--- backend/uBLOG/api/models.py
+++ backend/uBLOG/api/models.py
@@ -24,7 +24,7 @@ class Comment(models.Model):
         
 class Category(models.Model):
     name = models.CharField(max_length=100, blank=True, default='')
-    owner = models.ForeignKey('auth.User', related_name='categories', on_delete=models.CASCADE)
+    author = models.ForeignKey('auth.User', related_name='categories', on_delete=models.CASCADE)
     blogs = models.ManyToManyField('Blog', related_name='categories', blank=True)
     
     class Meta:
