3377ef55afd240d8b425a17752f38cfc4ea7bd78
dotSIS <sjezzrey@gmail.com>
2022-05-06 22:05:56 +0800

    added category model

--- backend/uBLOG/api/models.py
+++ backend/uBLOG/api/models.py
@@ -21,3 +21,11 @@ class Comment(models.Model):
     
     class Meta:
         ordering = ['commented']
+        
+class Category(models.Model):
+    name = models.CharField(max_length=100, blank=True, default='')
+    owner = models.ForeignKey('auth.User', related_name='categories', on_delete=models.CASCADE)
+    blogs = models.ManyToManyField('Blog', related_name='categories', blank=True)
+    
+    class Meta:
+        verbose_name_plural = 'categories'
